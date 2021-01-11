d71f6cffe654b262581d29b7360792e770cdab56
이동률 <@DESKTOP-IM1Q45A>
2021-01-11 15:37:31 +0900

    오류체크

--- src/main/java/com/mert/controller/UserController.java
+++ src/main/java/com/mert/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.mert.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Yasin Mert on 25.02.2017.
  */
@@ -10,10 +13,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mert.model.User;
@@ -33,19 +36,21 @@ public class UserController {
 	
 	//toast ui grid test 
 	@RequestMapping(value = "/all-test")
-	public ModelAndView gridTest(User search) {  //User model 로 데이터 매칭 바인딩
+	public @ResponseBody ModelAndView gridTest(User search) {  //User model 로 데이터 매칭 바인딩
 		System.out.println("all-test실행");
-		System.out.println(search.getName() + "name 변수가전달됌");
+		System.out.println(search.getName() + "name 변수가전달됌22");
 		ModelAndView modelAndView = new ModelAndView();
 		//POINT=7 http://stackoverflow.com/questions/22364886/neither-bindingresult-nor-plain-target-object-for-bean-available-as-request-attr
-		modelAndView.addObject("users", userService.findAll());
-		modelAndView.addObject("mode", "MODE_ALL");
+
+		modelAndView.addObject("mode", "MODE_return");
+		modelAndView.addObject("user_data", search.getName());
 		modelAndView.addObject("auth", getUser());
 		modelAndView.addObject("control", getUser().getRole().getRole());
 		modelAndView.setViewName("user");
+		
+		System.out.println(modelAndView.getModel());
 		return modelAndView;
 	}
-
 
 	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	public ModelAndView allUsers() {
